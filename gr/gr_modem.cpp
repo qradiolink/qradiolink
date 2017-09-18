c962feaaa9d49deb3fce5bcce00ff30754f94cb6
Adrian Musceac <kantooon@gmail.com>
2017-09-18 14:39:20 +0300

    Adjust rates

--- gr/gr_modem.cpp
+++ gr/gr_modem.cpp
@@ -106,7 +106,7 @@ void gr_modem::initTX(int modem_type)
     }
     else if(modem_type == gr_modem_types::ModemTypeQPSKVideo)
     {
-        _gr_mod_qpsk_sdr = new gr_mod_qpsk_sdr(0, 2, 1920000, 1700, 480000, 1, _requested_frequency_hz, 50);
+        _gr_mod_qpsk_sdr = new gr_mod_qpsk_sdr(0, 2, 1474800, 1700, 370000, 1, _requested_frequency_hz, 50);
         _frame_length = 7997;
         //_gr_mod_qpsk_sdr->start();
     }
@@ -176,7 +176,7 @@ void gr_modem::initRX(int modem_type)
     else if (modem_type == gr_modem_types::ModemTypeQPSKVideo)
     {
         _gr_demod_qpsk_sdr = new gr_demod_qpsk_sdr(
-                    _const_gui,_rssi_gui, 0,2,2000000,1474800,1700,240000,1, _requested_frequency_hz, 50);
+                    _const_gui,_rssi_gui, 0,2,2000000,1474800,1700,370000,1, _requested_frequency_hz, 50);
         _bit_buf_len = 7997 *8;
         _frame_length = 7997;
         _bit_buf = new unsigned char[_bit_buf_len];
