c7d86ff6039ebeaf15e7be9c2ebe6778520948c9
Adrian Musceac <kantooon@gmail.com>
2017-09-26 22:11:04 +0300

    tests

--- gr/gr_demod_bpsk_sdr.cpp
+++ gr/gr_demod_bpsk_sdr.cpp
@@ -61,8 +61,8 @@ gr_demod_bpsk_sdr::gr_demod_bpsk_sdr(gr::qtgui::sink_c::sptr fft_gui, gr::qtgui:
     _complex_to_real = gr::blocks::complex_to_real::make();
     _binary_slicer = gr::digital::binary_slicer_fb::make();
     _diff_decoder = gr::digital::diff_decoder_bb::make(2);
-    gr::fec::code::cc_decoder::sptr cc_decoder = gr::fec::code::cc_decoder::make(512,7,2,polys,
-                                                                                 0,-1,CC_TRUNCATED);
+    gr::fec::code::cc_decoder::sptr cc_decoder = gr::fec::code::cc_decoder::make(160,7,2,polys,
+                                                                                 0,-1,CC_TERMINATED);
     _fec_decoder = gr::fec::decoder::make(cc_decoder,1,1);
     _multiply_const_fec = gr::blocks::multiply_const_ff::make(48.0);
     _float_to_uchar = gr::blocks::float_to_uchar::make();
