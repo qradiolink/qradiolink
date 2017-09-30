6263649f4c101d7c4f2d03a76d0200123755f332
Adrian Musceac <kantooon@gmail.com>
2017-09-30 21:29:42 +0300

    WIP

--- gr/gr_mod_nbfm_sdr.cpp
+++ gr/gr_mod_nbfm_sdr.cpp
@@ -21,7 +21,7 @@ gr_mod_nbfm_sdr::gr_mod_nbfm_sdr(QObject *parent, int samp_rate, int carrier_fre
                     1, 48000, _filter_width, 600, gr::filter::firdes::WIN_HAMMING));
 
     std::vector<float> interp_taps = gr::filter::firdes::low_pass(1, 48000,
-                                                        2500, 10000);
+                                                        _filter_width, 2000);
     float rerate = (float)_samp_rate/48000.0;
     _resampler = gr::filter::pfb_arb_resampler_ccf::make(rerate, interp_taps, 32);
     _amplify = gr::blocks::multiply_const_cc::make(10,1);
