dbf47f8f710e67941e5c4eba0d2244f27db3fe28
Adrian Musceac <kantooon@gmail.com>
2017-11-01 10:29:47 +0200

    4FSK clock sync

--- gr/gr_demod_4fsk_sdr.cpp
+++ gr/gr_demod_4fsk_sdr.cpp
@@ -75,7 +75,7 @@ gr_demod_4fsk_sdr::gr_demod_4fsk_sdr(gr::qtgui::sink_c::sptr fft_gui, gr::qtgui:
     _symbol_filter = gr::filter::fft_filter_ccf::make(1,symbol_filter_taps);
     float gain_mu = 0.025;
     _clock_recovery = gr::digital::clock_recovery_mm_cc::make(_samples_per_symbol, 0.025*gain_mu*gain_mu, 0.5, gain_mu,
-                                                              0.055);
+                                                              0.001);
     _multiply_symbols = gr::blocks::multiply_const_cc::make(0.15);
     _diff_decoder = gr::digital::diff_decoder_bb::make(4);
     _map = gr::digital::map_bb::make(map);
