3855915215aa56a55ab81e1445b171f29f079f75
Adrian Musceac <kantooon@gmail.com>
2017-11-01 10:21:50 +0200

    Clock sync QPSK 2k

--- gr/gr_demod_qpsk_sdr.cpp
+++ gr/gr_demod_qpsk_sdr.cpp
@@ -84,8 +84,8 @@ gr_demod_qpsk_sdr::gr_demod_qpsk_sdr(gr::qtgui::sink_c::sptr fft_gui, gr::qtgui:
     float gain_mu, omega_rel_limit;
     if(_target_samp_rate == 20000)
     {
-        gain_mu = 0.05;
-        omega_rel_limit = 0.025;
+        gain_mu = 0.025;
+        omega_rel_limit = 0.0005;
     }
     else
     {
