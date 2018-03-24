4ad49ec6d9bc4db67a6a8b99ee2ef7ce32a775f9
Adrian Musceac <kantooon@gmail.com>
2018-03-24 16:13:14 +0200

    msec 47500

--- radioop.cpp
+++ radioop.cpp
@@ -303,7 +303,7 @@ void RadioOp::processNetStream()
 {
     qint64 microsec;
     microsec = (quint64)_data_read_timer->nsecsElapsed()/1000;
-    if(microsec < 48000)
+    if(microsec < 47500)
     {
         return;
     }
