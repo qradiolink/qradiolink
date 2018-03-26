f291c39e705f7bcc3ceb8c6665b6b0128065a08e
Adrian Musceac <kantooon@gmail.com>
2018-03-26 11:25:06 +0300

    Adjust loop time for data modem

--- radioop.cpp
+++ radioop.cpp
@@ -306,7 +306,7 @@ void RadioOp::processNetStream()
 {
     qint64 microsec;
     microsec = (quint64)_data_read_timer->nsecsElapsed()/1000;
-    if(microsec < 47500)
+    if(microsec < 47200)
     {
         return;
     }
