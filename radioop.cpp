35929393d3cc11d24c3f01988c9ccc25124d45a4
Adrian Musceac <kantooon@gmail.com>
2018-03-27 23:29:21 +0300

    adjust latency

--- radioop.cpp
+++ radioop.cpp
@@ -310,10 +310,10 @@ int RadioOp::processVideoStream(bool &frame_flag)
 void RadioOp::processNetStream()
 {
     // 48400
-    qint64 time_per_frame = 48400;
+    qint64 time_per_frame = 48500;
     qint64 microsec, time_left;
     microsec = (quint64)_data_read_timer->nsecsElapsed()/1000;
-    if(microsec < 46400)
+    if(microsec < 46500)
     {
         return;
     }
