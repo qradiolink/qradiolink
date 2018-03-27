711eb6816906a2669be6115fa581a24c61c3bd1c
Adrian Musceac <kantooon@gmail.com>
2018-03-27 20:28:34 +0300

    Adjust sleep timer

--- radioop.cpp
+++ radioop.cpp
@@ -307,10 +307,10 @@ int RadioOp::processVideoStream(bool &frame_flag)
 void RadioOp::processNetStream()
 {
     // 48400
-    qint64 time_per_frame = 48608;
+    qint64 time_per_frame = 48550;
     qint64 microsec, time_left;
     microsec = (quint64)_data_read_timer->nsecsElapsed()/1000;
-    if(microsec < 46608)
+    if(microsec < 46550)
     {
         return;
     }
