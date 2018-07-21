2ba7afd1021372a7825e10acaff7f1f84e191e85
Adrian Musceac <kantooon@gmail.com>
2018-07-22 00:23:13 +0300

    Reduced RRC filter size QPSK

--- gr/gr_mod_qpsk_sdr.cpp
+++ gr/gr_mod_qpsk_sdr.cpp
@@ -42,7 +42,7 @@ gr_mod_qpsk_sdr::gr_mod_qpsk_sdr(int sps, int samp_rate, int carrier_freq,
     _carrier_freq = carrier_freq;
     _filter_width = filter_width;
     int filter_slope = 1200;
-    int filt_length = 512;
+    int filt_length = 128;
     if(sps < 10)
         filter_slope = 5000;
     if(sps > 100)
