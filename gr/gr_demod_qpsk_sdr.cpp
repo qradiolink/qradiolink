ac57d38774463d86c8af597522ed916a17173d24
Adrian Musceac <kantooon@gmail.com>
2018-08-15 01:37:19 +0300

    Add Doppler carrier tracking for narrow QPSK modes

--- gr/gr_demod_qpsk_sdr.cpp
+++ gr/gr_demod_qpsk_sdr.cpp
@@ -40,13 +40,21 @@ gr_demod_qpsk_sdr::gr_demod_qpsk_sdr(std::vector<int>signature, int sps, int sam
     int decimation;
     int interpolation;
     int filt_bandwidth;
-    if(sps > 4)
+    if(sps > 4 && sps < 125)
     {
         interpolation = 1;
-        decimation = 50;
-        _samples_per_symbol = sps*2/25;
-        _target_samp_rate = 20000;
-        filt_bandwidth = _target_samp_rate;
+        decimation = 25;
+        _samples_per_symbol = sps*4/25;
+        _target_samp_rate = 40000;
+        filt_bandwidth = 5000;
+    }
+    else if(sps >= 125)
+    {
+        interpolation = 1;
+        decimation = 100;
+        _samples_per_symbol = sps/25;
+        _target_samp_rate = 10000;
+        filt_bandwidth = 2000;
     }
     else
     {
@@ -80,11 +88,11 @@ gr_demod_qpsk_sdr::gr_demod_qpsk_sdr(std::vector<int>signature, int sps, int sam
                 constellation->points(),pre_diff_code,4,2,2,1,1,const_map);
     */
 
-    std::vector<float> taps = gr::filter::firdes::low_pass(1, _samp_rate, _filter_width, filt_bandwidth);
+    std::vector<float> taps = gr::filter::firdes::low_pass(1, _samp_rate, _target_samp_rate, filt_bandwidth);
 
     _resampler = gr::filter::rational_resampler_base_ccf::make(interpolation, decimation, taps);
 
-    _agc = gr::analog::agc2_cc::make(1e-1, 1e-1, 1, 0);
+    _agc = gr::analog::agc2_cc::make(1e-1, 1e-1, 1, 1);
     _filter = gr::filter::fft_filter_ccf::make(1, gr::filter::firdes::low_pass(
                                 1, _target_samp_rate, _filter_width, filter_slope,gr::filter::firdes::WIN_BLACKMAN_HARRIS) );
     float gain_mu, omega_rel_limit;
@@ -107,13 +115,13 @@ gr_demod_qpsk_sdr::gr_demod_qpsk_sdr(std::vector<int>signature, int sps, int sam
 
     _shaping_filter = gr::filter::fft_filter_ccf::make(
                 1, gr::filter::firdes::root_raised_cosine(1,_target_samp_rate,_target_samp_rate/_samples_per_symbol,0.35,32));
-    _clock_recovery = gr::digital::clock_recovery_mm_cc::make(_samples_per_symbol, 0.025*gain_mu*gain_mu, 0.5, gain_mu,
+    _clock_recovery = gr::digital::clock_recovery_mm_cc::make(_samples_per_symbol, 0.025*gain_mu*gain_mu, 0, gain_mu,
                                                               omega_rel_limit);
     std::vector<float> pfb_taps = gr::filter::firdes::root_raised_cosine(flt_size,flt_size, 1, 0.35, flt_size * 11 * _samples_per_symbol);
     _clock_sync = gr::digital::pfb_clock_sync_ccf::make(_samples_per_symbol,0.0628,pfb_taps);
-    _costas_loop = gr::digital::costas_loop_cc::make(2*M_PI/100,4);
-    _equalizer = gr::digital::cma_equalizer_cc::make(8,1,0.00005,1);
-    _fll = gr::digital::fll_band_edge_cc::make(_samples_per_symbol, 0.35, filt_length, 2*M_PI/100);
+    _costas_loop = gr::digital::costas_loop_cc::make(2*M_PI/100,4,true);
+    _equalizer = gr::digital::cma_equalizer_cc::make(8,1,0.005,1);
+    _fll = gr::digital::fll_band_edge_cc::make(_samples_per_symbol, 0.35, filt_length, 12*M_PI/100);
     _descrambler = gr::digital::descrambler_bb::make(0x8A, 0x7F ,7);
 
     gr::fec::code::cc_decoder::sptr decoder = gr::fec::code::cc_decoder::make(80, 7, 2, polys);
@@ -130,18 +138,10 @@ gr_demod_qpsk_sdr::gr_demod_qpsk_sdr(std::vector<int>signature, int sps, int sam
 
 
     connect(self(),0,_resampler,0);
-    connect(_resampler,0,_filter,0);
+    connect(_resampler,0,_fll,0);
+    connect(_fll,0,_filter,0);
     connect(_filter,0,self(),0);
-    if(sps > 2)
-    {
-        connect(_filter,0,_fll,0);
-        //connect(_shaping_filter,0,_agc,0);
-        connect(_fll,0,_agc,0);
-    }
-    else
-    {
-        connect(_filter,0,_agc,0);
-    }
+    connect(_filter,0,_agc,0);
 
     connect(_agc,0,_clock_recovery,0);
     connect(_clock_recovery,0,_equalizer,0);
