56e524742b7a4acf54da4d89d0ae3cf91815687c
Adrian Musceac <kantooon@gmail.com>
2018-12-30 04:20:31 +0200

    Increase SSB amplification

--- gr/gr_mod_ssb_sdr.cpp
+++ gr/gr_mod_ssb_sdr.cpp
@@ -36,7 +36,7 @@ gr_mod_ssb_sdr::gr_mod_ssb_sdr(int sps, int samp_rate, int carrier_freq,
     _carrier_freq = carrier_freq;
     _filter_width = filter_width;
 
-    _agc = gr::analog::agc2_ff::make(1e-1, 1e-3, 1, 1);
+    _agc = gr::analog::agc2_ff::make(1e-1, 1e-1, 1, 0);
     _audio_filter = gr::filter::fft_filter_fff::make(
                 1,gr::filter::firdes::band_pass(
                     1, target_samp_rate, 200, _filter_width, 50, gr::filter::firdes::WIN_BLACKMAN_HARRIS));
@@ -49,7 +49,7 @@ gr_mod_ssb_sdr::gr_mod_ssb_sdr(int sps, int samp_rate, int carrier_freq,
                                                         _filter_width, 1200);
 
     _resampler = gr::filter::rational_resampler_base_ccf::make(500,4, interp_taps);
-    _amplify = gr::blocks::multiply_const_cc::make(0.25,1);
+    _amplify = gr::blocks::multiply_const_cc::make(0.5,1);
     _bb_gain = gr::blocks::multiply_const_cc::make(1,1);
     _filter_usb = gr::filter::fft_filter_ccc::make(
                 1,gr::filter::firdes::complex_band_pass_2(
