a1b09433666049b9603f827c79c28f94259d613a
Adrian Musceac <kantooon@gmail.com>
2019-10-29 17:43:30 +0200

    travis update

--- travis/script.sh
+++ travis/script.sh
@@ -502,20 +502,19 @@ fi
 rm -f "${DEBC}"
 
 VER_INFO=$(grep "upstream-tag" "${TRAVIS_DEBIAN_TARGET_DIR}"/debian/gbp.conf | awk -F' ' {'print $3'})
-export VERSION=${VER_INFO}
 
 docker exec "$(cat "${CIDFILE}")" dpkg-deb -x "${TRAVIS_DEBIAN_BUILD_DIR}/qradiolink_${VER_INFO}_amd64.deb" ${TRAVIS_DEBIAN_BUILD_DIR}
+docker exec "$(cat "${CIDFILE}")" export VERSION=${VER_INFO}
 docker exec "$(cat "${CIDFILE}")" ./linuxdeploy-x86_64.AppImage --appdir AppDir -e ${TRAVIS_DEBIAN_BUILD_DIR}/usr/bin/qradiolink -i ${BUILD_PATH}/res/icon.png -d ${BUILD_PATH}/qradiolink.desktop --plugin qt --output appimage
+docker exec "$(cat "${CIDFILE}")" echo $(pwd)
+docker exec "$(cat "${CIDFILE}")" ls -la
 
 Info "Removing container"
 docker rm -f "$(cat "${CIDFILE}")" >/dev/null
 rm -f "${CIDFILE}"
 
-#rm -f ./linuxdeploy-x86_64.AppImage
-#rm -f ./linuxdeploy-plugin-qt-x86_64.AppImage
-echo $(pwd)
-ls -al
-ls -al "${TRAVIS_DEBIAN_TARGET_DIR}"
+rm -f ./linuxdeploy-x86_64.AppImage
+rm -f ./linuxdeploy-plugin-qt-x86_64.AppImage
 
 
 #  _                   _          _      _     _                          _
