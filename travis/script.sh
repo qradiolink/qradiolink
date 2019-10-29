91bdf9cb13420333e0db7a10df194f141b305941
Adrian Musceac <kantooon@gmail.com>
2019-10-29 17:52:23 +0200

    travis update

--- travis/script.sh
+++ travis/script.sh
@@ -504,7 +504,6 @@ rm -f "${DEBC}"
 VER_INFO=$(grep "upstream-tag" "${TRAVIS_DEBIAN_TARGET_DIR}"/debian/gbp.conf | awk -F' ' {'print $3'})
 
 docker exec "$(cat "${CIDFILE}")" dpkg-deb -x "${TRAVIS_DEBIAN_BUILD_DIR}/qradiolink_${VER_INFO}_amd64.deb" ${TRAVIS_DEBIAN_BUILD_DIR}
-docker exec "$(cat "${CIDFILE}")" export VERSION=${VER_INFO}
 docker exec "$(cat "${CIDFILE}")" ./linuxdeploy-x86_64.AppImage --appdir AppDir -e ${TRAVIS_DEBIAN_BUILD_DIR}/usr/bin/qradiolink -i ${BUILD_PATH}/res/icon.png -d ${BUILD_PATH}/qradiolink.desktop --plugin qt --output appimage
 docker exec "$(cat "${CIDFILE}")" echo $(pwd)
 docker exec "$(cat "${CIDFILE}")" ls -la
