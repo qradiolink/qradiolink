972918a63ce9ec66abbde513121e9b2bf77350e7
Adrian Musceac <kantooon@gmail.com>
2019-12-11 21:03:40 +0200

    infect code with license

--- src/limits.cpp
+++ src/limits.cpp
@@ -1,7 +1,24 @@
+// Written by Adrian Musceac YO8RZZ , started March 2019.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 #include "limits.h"
 
 Limits::Limits()
 {
+    _allocation_name = "IARU region 1 / CEPT allocation";
     // TODO: configurable band allocation
     _tx_limits.push_back(std::complex<long long>(1810000, 2000000));
     _tx_limits.push_back(std::complex<long long>(3500000, 3800000));
@@ -28,6 +45,8 @@ Limits::Limits()
 
 bool Limits::checkLimit(long long tx_freq)
 {
+    /// this doesn't take into account the width of the signal,
+    /// just the carrier center check
     for(unsigned int i=0;i<_tx_limits.size();i++)
     {
         std::complex<long long> band = _tx_limits.at(i);
--- src/limits.h
+++ src/limits.h
@@ -1,8 +1,25 @@
+// Written by Adrian Musceac YO8RZZ , started March 2019.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 #ifndef LIMITS_H
 #define LIMITS_H
 
 #include <vector>
 #include <complex>
+#include <map>
 
 class Limits
 {
@@ -12,6 +29,7 @@ public:
 
 private:
     std::vector<std::complex<long long>> _tx_limits;
+    std::string _allocation_name;
 };
 
 #endif // LIMITS_H
