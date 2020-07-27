ff7ab06c16e798e5feddf94ab353769b8a452774
Adrian Musceac <kantooon@gmail.com>
2020-07-27 15:57:20 +0300

    Fix regression in PSK receivers (CMA equalizer wrong params)

--- gr/gr_demod_qpsk_sdr.cpp
+++ gr/gr_demod_qpsk_sdr.cpp
@@ -127,7 +127,7 @@ gr_demod_qpsk_sdr::gr_demod_qpsk_sdr(std::vector<int>signature, int sps, int sam
     _clock_sync = gr::digital::pfb_clock_sync_ccf::make(_samples_per_symbol,
                                                     2*M_PI/100,pfb_taps,flt_size, 0, 1.01, 8);
     _costas_loop = gr::digital::costas_loop_cc::make(2*M_PI/200,4,true);
-    _equalizer = gr::digital::cma_equalizer_cc::make(8,2,0.0005,1);
+    _equalizer = gr::digital::cma_equalizer_cc::make(8,2,0.00005,1);
 
     _diff_phasor = gr::digital::diff_phasor_cc::make();
     const std::complex<float> i(0, 1);
