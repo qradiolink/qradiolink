6ead39e180d0cd4892210ae8aa31006f18752ce7
Adrian Musceac <kantooon@gmail.com>
2020-09-09 19:51:10 +0300

    Re-enable AppImage build

--- travis/script.sh
+++ travis/script.sh
@@ -68,9 +68,9 @@ then
 fi
 
 # download linuxdeploy and its Qt plugin
-# wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-# wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
-# chmod +x linuxdeploy*.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+chmod +x linuxdeploy*.AppImage
 
 
 Info "Starting build of ${SOURCE} using travis.debian.net"
@@ -504,15 +504,15 @@ rm -f "${DEBC}"
 VER_INFO=$(grep "upstream-tag" "${TRAVIS_DEBIAN_TARGET_DIR}"/debian/gbp.conf | awk -F' ' {'print $3'})
 
 docker exec "$(cat "${CIDFILE}")" dpkg-deb -x "${TRAVIS_DEBIAN_BUILD_DIR}/qradiolink_${VER_INFO}_amd64.deb" ${TRAVIS_DEBIAN_BUILD_DIR}
-# docker exec --env VERSION=${VER_INFO} "$(cat "${CIDFILE}")" ./linuxdeploy-x86_64.AppImage --appdir AppDir -e ${TRAVIS_DEBIAN_BUILD_DIR}/usr/bin/qradiolink -i ${BUILD_PATH}/res/icon.png -d ${BUILD_PATH}/qradiolink.desktop --plugin qt --output appimage
-# docker cp "$(cat "${CIDFILE}")":"${TRAVIS_DEBIAN_TARGET_DIR}"/QRadioLink-${VER_INFO}-x86_64.AppImage "${TRAVIS_DEBIAN_TARGET_DIR}"
+docker exec --env VERSION=${VER_INFO} "$(cat "${CIDFILE}")" ./linuxdeploy-x86_64.AppImage --appdir AppDir -e ${TRAVIS_DEBIAN_BUILD_DIR}/usr/bin/qradiolink -i ${BUILD_PATH}/res/icon.png -d ${BUILD_PATH}/qradiolink.desktop --plugin qt --output appimage
+docker cp "$(cat "${CIDFILE}")":"${TRAVIS_DEBIAN_TARGET_DIR}"/QRadioLink-${VER_INFO}-x86_64.AppImage "${TRAVIS_DEBIAN_TARGET_DIR}"
 
 Info "Removing container"
 docker rm -f "$(cat "${CIDFILE}")" >/dev/null
 rm -f "${CIDFILE}"
 
-# rm -f ./linuxdeploy-x86_64.AppImage
-# rm -f ./linuxdeploy-plugin-qt-x86_64.AppImage
+rm -f ./linuxdeploy-x86_64.AppImage
+rm -f ./linuxdeploy-plugin-qt-x86_64.AppImage
 
 
 #  _                   _          _      _     _                          _
