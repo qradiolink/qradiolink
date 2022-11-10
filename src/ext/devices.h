80bae614d157955a9d3b09517773f52205545894
adrian <adrian@qradiolink.org>
2022-11-10 11:34:37 +0200

    Add notice regarding repository move

--- src/ext/devices.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEVICES_H
-#define DEVICES_H
-#include <QList>
-#include <string>
-
-void findLimeDevices(QList<QString> &devices);
-int findSoapyDevices(const std::string &argStr, const bool sparse, QList<QString> &devices);
-
-#endif // DEVICES_H
